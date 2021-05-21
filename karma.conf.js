906073146778bd2a0e311a5eaaa97fdd0d077dfd
Mike <michael.d.keyes@gmail.com>
2021-05-21 10:00:22 -0400

    Test config

--- karma.conf.js
+++ karma.conf.js
@@ -3,14 +3,14 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage"),
+      require("@angular-devkit/build-angular/plugins/karma"),
     ],
     client: {
       jasmine: {
@@ -19,26 +19,49 @@ module.exports = function (config) {
         // for example, you can disable the random execution with `random: false`
         // or set a specific seed with `seed: 4321`
       },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-jenkins-test'),
-      subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
+      dir: require("path").join(__dirname, "./coverage/angular-jenkins-test"),
+      subdir: ".",
+      reporters: [{ type: "html" }, { type: "text-summary" }],
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ["progress", "kjhtml"],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ["ChromeHeadlessNoSandbox"],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: "ChromeHeadless",
+        flags: [
+          "--no-sandbox",
+          // required to run without privileges in Docker
+          "--disable-web-security",
+          "--disable-gpu",
+          "--remote-debugging-port=9222",
+        ],
+      },
+    },
     singleRun: false,
-    restartOnFileChange: true
+    junitReporter: {
+      outputDir: "test-reports",
+      // results will be saved as $outputDir/$browserName.xml
+      outputFile: "junit-report.xml",
+      // if included,results will be saved as $outputDir/$browserName/$outputFile
+      suite: "",
+      // suite will become the package name attribute in xml testsuite element
+      useBrowserName: false,
+      // add browser name to report and classes names
+      nameFormatter: undefined,
+      // function (browser,result) to customize the name attribute in xml testcase element
+      classNameFormatter: undefined,
+      // function (browser,result) to customize the classname attribute in xml testcase element
+      properties: {}, // key value pair of properties to add to the section of the report
+    },
   });
 };
